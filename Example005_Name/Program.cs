4c6d12452473b3947d47fa8a05879801a1816a1e
Yuriy <sol-yuriy@yandex.ru>
2023-01-15 16:26:06 +0500

    Добавил ввод имени и реакцию

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Юрий")
+{
+    Console.WriteLine("Ура, это же Юрий!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
