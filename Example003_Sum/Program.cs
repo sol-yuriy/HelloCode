2f952134f64b807cade2b7b3f8fa766a87f49421
Yuriy <sol-yuriy@yandex.ru>
2023-01-15 15:08:44 +0500

    Добавил проект с делением

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿int numberA = 3;
 int numberB = 6;
 int sum = numberA + numberB;
+Console.Write("Сумма равна: ");
 Console.WriteLine(sum);
---
+++ Example004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿double num1 = 12;
+double num2 = 5;
+double div = num1 / num2;
+Console.WriteLine(div);
