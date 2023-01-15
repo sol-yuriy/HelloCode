795fb1852218efab7850b5ed13d20c1fc91085d7
Yuriy <sol-yuriy@yandex.ru>
2023-01-15 15:41:24 +0500

    Добавил в сумму случайные числа

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 6;
+﻿int numberA = new Random().Next(1, 15);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.Write("Сумма равна: ");
 Console.WriteLine(sum);
